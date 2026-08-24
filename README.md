106c955be83e5f18bcd1a6a7447552f1159e7692
Yoav321 <yoav.bierkatz@gmail.com>
2026-08-24 10:29:38 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PRIMM: 1+1 = 11?
+In this PRIMM activity, you will learn about data types and the arithmetic operators that Python offers. Read along in the REFLECTIONS.md file and enter your reflections and answers to the questions.
---
+++ primm_11.py
@@ -0,0 +1,15 @@
+"""
+Description of program here
+First Last - Month Year
+"""
+
+def main() -> None:
+  
+    num1: int = input("Enter a number: ")
+    num2: int = input("Enter another number: ")
+    result: int = num1 + num2
+
+    print(f"{num1} + {num2} = {result}")
+
+if __name__ == "__main__":
+  main()
